1625dfb329c955fc56cff7b63987280a155d67cd
Blake Bless <blakebless7@gmail.com>
2016-10-11 16:11:01 -0700

    add fade in and out effect to navbar, and titles to videos.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
   var videoArray = [];
 
 
-  function Video( videoThumbnail, videoId, videoRelevancy){
+  function Video( videoThumbnail, videoId, videoRelevancy, videoTitle){
     this.videoThumbnail = videoThumbnail;
     this.videoId = videoId;
     this.videoRelevancy = videoRelevancy;
+    this.videoTitle = videoTitle;
   }
 
   $.get(
@@ -35,9 +36,9 @@ $(document).ready(function(){
       function(data){
         var output;
         $.each(data.items, function(i, item) {
-          var newVideo = new Video(item.snippet.thumbnails.high.url, item.snippet.resourceId.videoId, item.snippet.position);
+          var newVideo = new Video(item.snippet.thumbnails.high.url, item.snippet.resourceId.videoId, item.snippet.position, item.snippet.title);
           videoArray.push(newVideo);
-          $('.showcase').append('<div class="video" id="'+ newVideo.videoRelevancy +'"><div class="' + newVideo.videoRelevancy + '-video-thumbnail"><img src="' + newVideo.videoThumbnail + '"/></div><div class="' + newVideo.videoRelevancy + '-video-overlay video-overlay"><div class=" video-modal"><iframe width="950" height="500" src="https://www.youtube.com/embed/' + newVideo.videoId + '" frameborder="0" allowfullscreen></iframe></div><div><p class="' + newVideo.videoRelevancy + '-close-overlay close-overlay"></p></div></div></div>');
+          $('.showcase').append('<div class="video" id="'+ newVideo.videoRelevancy +'"><div class="video-title font1">' + newVideo.videoTitle + '</div><div class="' + newVideo.videoRelevancy + '-video-thumbnail"><img src="' + newVideo.videoThumbnail + '" class="video-thumbnail"/></div><div class="' + newVideo.videoRelevancy + '-video-overlay video-overlay"><div class=" video-modal"><iframe width="950" height="500" src="https://www.youtube.com/embed/' + newVideo.videoId + '" frameborder="0" allowfullscreen></iframe></div><div><p class="' + newVideo.videoRelevancy + '-close-overlay close-overlay"></p></div></div></div>');
           $('.' + newVideo.videoRelevancy + '-video-overlay').hide();
 
           $('#' + newVideo.videoRelevancy + '').click(function() {
@@ -51,4 +52,13 @@ $(document).ready(function(){
       }
     );
   }
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() < $('.welcome').height()) {
+      $('.nav-bar').addClass('opac-bar');
+    }
+    if($(window).scrollTop() >= $('.welcome').height()) {
+      $('.nav-bar').removeClass('opac-bar');
+    }
+  });
 });
